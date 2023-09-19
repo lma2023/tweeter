ac4fe55e107c1e79980286ed39e8534b19e7f0f3
lma2023 <lyassovam@gmail.com>
2023-09-19 14:00:28 -0600

    Creating 'escape' function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,20 +3,25 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const escapeString = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+};
 
-
-    const renderTweets = (data) => {
-        // loops through tweets
-        for (let tweet of data) {
-            // calls createTweetElement for each tweet
-            const $tweet = createTweetElement(tweet);
-            // takes return value and appends it to the tweets container
-            $('#tweets-container').append($tweet);
-        }
-    };
-    const createTweetElement = (data) => {
-        const created = timestamp(data.created_at);
-        const tweetHTML = $(`
+const renderTweets = (data) => {
+    $('#tweet-text').val('');
+    $('#tweet-container').empty();
+    for (let tweet of data) {
+        // calls createTweetElement for each tweet
+        const $tweet = createTweetElement(tweet);
+        // takes return value and appends it to the tweets container
+        $('#tweet-container').prepend($tweet);
+    }
+};
+const createTweetElement = (data) => {
+    // const {user, content, created_at} = data;
+    const tweetHTML = $(`
         <article class="tweet">
         <div class="header">
           <div class="user">
@@ -26,7 +31,7 @@
           <span class="tag">${data.user.handle}</span>
         </div>
         <div class="body">
-          <span>${data.content.text}</span>
+          <span>${escapeString(data.content.text)}</span>
         </div>
         <div class="footer">
         <span>${timeago.format(data.created_at)}</span>
@@ -34,51 +39,51 @@
         </div>
       </article>
       `)
-        return tweetHTML;
-    };
+    return tweetHTML;
+};
 
-    const timestamp = (time) => {
-        const timestamp = time;
-        const today = new Date();
-        const postDay = new Date(timestamp);
-
-        // Time differences
-        const timeDiff = today.getTime() - postDay.getTime();
-
-        // Convert to days
-        return Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-    };
-    //   renderTweets(data);
-
-    // -- AJAX GET request 
-    // const loadTweets = () => {
-    //     $.get("/tweets", function (data) {
-    //         renderTweets(data);
-    //     });
-    // };
-
-    function loadTweets() {
-        $.ajax("/tweets", {
-          method: "GET"})
-          .then(function(tweets) {
+function loadTweets() {
+    $.ajax("/tweets", {
+        method: "GET"
+    })
+        .then(function (tweets) {
             renderTweets(tweets);
-          });
-      }
-    //   New tweet submission
-    $(document).ready(function () {
-    $("#post-tweet").on("submit", function (event) {
+        });
+}
+//   New tweet submission
+$(document).ready(function () {
+    $("#post-tweet").submit(function (event) {
         event.preventDefault();
-        console.log("New tweet")
+        // console.log("New tweet")
+        $("#error-message").slideUp();
 
-        $.ajax("/tweets", {
-            method: "POST",
-            data: $("#post-tweet").serialize(),
-            success: () => {
-                loadTweets()}
-        })
-      });
-    loadTweets();
-  });
+        const value = $(this).find("#tweet-text").val();
+
+        // -- Displaying validation errors with jQuery
+        if ($("#tweet-text").val().trim().length === 0) {
+            $("#err-msg").text("Empty field.");
+            $(".error").slideDown();
+            return;
+        }
+        if ($("#tweet-text").val().length > 140) {
+            $("#err-msg").text("Message exceding 140 characters.");
+            $(".error").slideDown();
+            return;
+        }
+
+        $(".error").hide();
+    
+            $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data: $("#post-tweet").serialize(),
+        success: () => {
+            loadTweets(), $(".counter").val("140")
+        }
+    });
+});
+loadTweets();
+});
 
 
 
